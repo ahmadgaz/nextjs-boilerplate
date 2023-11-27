045f05c30f5faca53d136fc283de85517c1bc695
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2023-11-27 00:14:16 -0800

    Initial Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'block',
+});
+export const lusitana = Lusitana({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'block',
+});
